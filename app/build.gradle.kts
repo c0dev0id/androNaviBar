01994365ba8c7ef3371a79a0531c19811b00d1c9
c0dev0id <sh+github@codevoid.de>
2026-03-02 08:38:38 +0100

    Commit Build Boilerplate

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "de.codevoid.andronavibar"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "de.codevoid.andronavibar"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
